b2e43b7ec6c828514e60fc34a7dc9de040f1acf7
hazy1993 <ldh2481@naver.com>
2023-03-27 00:16:09 +0900

    자동구성 정보 파일 분리

--- src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
+++ src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,41 @@
 package tobyspring.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[]{
-                "tobyspring.config.autoconfig.DispatcherServletConfig"
-                ,"tobyspring.config.autoconfig.TomcatWebServerConfig"
-        };
+
+        List<String> autoConfigs = new ArrayList<String>();
+      /*코드 변환 1.
+      for(String candidate: ImportCandidates.load(MyAutoConfigration.class, classLoader)){
+            autoConfigs.add(candidate);
+      }
+      코드변환 2.
+             ImportCandidates.load(MyAutoConfigration.class, classLoader).forEach(
+               candidate ->  autoConfigs.add(candidate)
+      );*/
+        //코드변환 3
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+//        return autoConfigs.stream().toArray(String[]::new); 스트림 이전
+        return autoConfigs.toArray(new String[0]);
+
+
+     /*간략하게
+       Iterable<String> candidate = ImportCandidates.load(MyAutoConfigration.class, classLoader);
+        return StreamSupport.stream(candidate.spliterator(),false).toArray(String[]::new);*/
     }
 }
